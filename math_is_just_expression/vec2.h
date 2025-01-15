7001101e42d66ae4c42d0566819562c99c777223
lolw <ADMIN@DESKTOP-CCL0V71>
2025-01-15 12:44:23 +0200

    vec2

--- math_is_just_expression/vec2.h
+++ math_is_just_expression/vec2.h
@@ -8,7 +8,13 @@ public:
 	float y = 0;
 
 	vec2(const float X, const float Y) :x(X), y(Y) {}
-	
+	//equate
+	bool operator==(const vec2& other)const {
+		return(x == other.x) && (y == other.y);
+	}
+	bool operator!=(const vec2& other)const {
+		return(x != other.x) && (y != other.y);
+	}
 	//addition
 	vec2 operator+(const vec2& rhs)const {
 		return vec2(x + rhs.x, y + rhs.y);
@@ -46,17 +52,20 @@ public:
 		return *this;
 	}
 
-	float Sqrt(const float num) {
+	static float Sqrt(const float num) {
+		static const float epsilon = 1e-6;
 		float temp = 0;
 		float sr = num / 2;
 
 		do {
 			temp = sr;
 			sr = (temp + (num / temp)) / 2;
-		} while ((temp - sr) != 0);
+		} while (std::abs(temp - sr) > epsilon);
 
 		return sr;
 	}
+
+
 	float DistSquared(const vec2& end)const {
 		const float A = (end.x - x) * 2;
 		const float B = (end.y - y) * 2;
@@ -68,6 +77,18 @@ public:
 		const float B = (end.y - y) * 2;
 		
 		return std::sqrtf(A + B);
+	}
+	float LenghtSquared()const {
+		return (x * x) + (y * y);
+	}
+	float Lenght()const {
+		return std::sqrtf((x * x) + (y * y));
+	}
+
+	vec2 Normalized()const {
+		float len = Lenght();
+		if (len == 0)return vec2(0, 0);
+		return { x / len, y / len };
 	}
 
 };
