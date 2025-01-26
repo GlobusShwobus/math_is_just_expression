a0fd5167b0fbecda51e22d5161ad75db0ef166f6
GlobusShwobus <fjodorzaro@gmail.com>
2025-01-26 20:29:06 +0200

    some shit done but meat is not cooked yet, implementing that JSON config is the biggest job honestly

--- math_is_just_expression/Entity.h
+++ math_is_just_expression/Entity.h
@@ -10,11 +10,13 @@ public:
 	float angle = 0;
 
 	CTransform(const vec2& POS, const vec2& VEL, float a) :pos(POS), vel(VEL), angle(a) {}
+	CTransform() = default;
 };
 
 class CShape {
 public:
 	sf::RectangleShape rect;
+	CShape() = default;
 	CShape(const sf::Vector2f& size, const sf::Color& fill, const sf::Color& outline, float thickness)
 		:rect(size) {
 		rect.setFillColor(fill);
@@ -27,6 +29,7 @@ class CCollision {
 public:
 	float radius = 0;
 	CCollision(float rad) :radius(rad) {}
+	CCollision() = default;
 };
 
 class CScore {
@@ -40,6 +43,7 @@ public:
 	int remaining = 0;
 	int total = 0;
 	CLifespan(int initial) :remaining(initial), total(initial) {}
+	CLifespan() = default;
 };
 
 class CInput {
@@ -48,6 +52,7 @@ public:
 	bool left = false;
 	bool right = false;
 	bool down = false;
+	bool shield = false;
 	bool shoot = false;
 
 	CInput() = default;
@@ -61,36 +66,89 @@ enum class EntityType {
 
 class Entity {
 
-	bool activeStatus = false;
+	bool activeStatus = true;
+	bool destroyStatus = false;
 	size_t id = 0;
 	EntityType type = EntityType::NULLTYPE;
 
 public:
 
-	Entity(const size_t id, const EntityType t);
-
+	Entity(const size_t ID, const EntityType t):id(ID), type(t) {}
 
 	CTransform    transform;
 	CShape        shape;
 	CCollision    colision;
-	CScore        score;
 	CLifespan     lifepoints;
-	CInput        input;
 
-	bool IsActive()const;
-	const EntityType Type()const;
-	const size_t ID()const;
-	void destroy();
-	void deactivate();
+
+	bool IsActive()const {
+		return activeStatus;
+	}
+	const EntityType Type()const {
+		return type;
+	}
+	const size_t ID()const {
+		return id;
+	}
+	void destroy() {
+		destroyStatus = true;
+	}
+
+	void deactivate() {
+		activeStatus = false;
+	}
+};
+
+//redefine score later to be read from JSON, basically remember the score, so don't use the  0 basically
+
+class Player: public Entity {
+
+public:
+
+	CScore score;
+	CInput input;
+
+	Player(size_t ID, EntityType type) :Entity(ID, type), score(0) {}
 
 };
 
+//manager will read the JSON config, remember configs and then be responsible for creating entities
+//spawning happens here and only here, instead of how it is in the course thus AddEntity will be void and just take param which to create
 class EntityManager {
 
 	std::unique_ptr<std::vector<Entity>> to_add;
-	std::unique_ptr<std::map<EntityType, std::vector<Entity>>> entities;
-
+	std::unique_ptr<std::vector<Entity>> entities;
 
 	size_t total_entities;
+
+	void RemoveInactive() {
+		entities->erase(std::remove_if(entities->begin(), entities->end(), [&](Entity& ent) {
+			return !ent.IsActive();
+			}), entities->end());
+	}
+
+public:
+
+	EntityManager(const size_t reserve_amount) {
+		to_add = std::make_unique<std::vector<Entity>>();
+		entities = std::make_unique<std::vector<Entity>>();
+		entities->reserve(reserve_amount);
+		to_add->reserve(reserve_amount);
+	}
+
+	void Update() {
+		//avoids iterator invalidation?
+		std::move(to_add->begin(), to_add->end(), std::back_inserter(*entities));
+		to_add->clear();
+		RemoveInactive();
+	}
+	void AddEntity(const EntityType type, const vec2& pos) {
+		//aint shit gonna happen untill we have JSON parsing done
+	}
+
+
+	std::vector<Entity>& GetEntities() {
+		return *entities;
+	}
 
 };
--- math_is_just_expression/Game.h
+++ math_is_just_expression/Game.h
@@ -2,26 +2,153 @@
 
 #include "Entity.h"
 
-/*
-
-
-set up the main rendering window in game::init, read window parameters from the config
-
-*/
-
-
 class Game {
 
 	sf::RenderWindow window;
-	EntityManager entities;
+
 	sf::Text score_text;
+
+	bool running = true;
+	bool paused = false;
+	size_t current_frame = 0;
+
+	EntityManager entities;
+	Player player;
 
 public:
 
 	void Init() {
 		//read from teh config json, the window one and set up window size, name, framrate
+
+		//window.create(sf::Videomode(x,y),"name")
+		//window.setFramerateLimit(xyz)
+
+		//spawn player
 	}
 	void Run() {
+		//add pause, some sys should work while paused, others not
+
+
+		while (running) {
+
+			entities.Update();
+
+			if (!paused) {
+				sEnemySpawner();
+				sMovement();
+				sCollision();
+			}
+			sUserInput();
+			sRender();
+
+			current_frame++;
+		}
 
 	}
+	void SetPaused() {
+		paused = !paused;
+	}
+
+	void sEnemySpawner() {
+		//entity manager is not set upp all the way to support this, but essentially call addEntity function
+	}
+
+	void sRender() {
+
+		window.clear();
+
+
+		//here draw other shit too
+
+		player.shape.rect.setPosition(player.transform.pos.x, player.transform.pos.y);
+
+
+		window.draw(player.shape.rect);
+		window.display();
+	}
+
+	void sUserInput() {
+
+		sf::Event sfevent;
+
+		while (window.pollEvent(sfevent)) {
+
+			//closes game
+			if (sfevent.type == sf::Event::Closed) {
+				running = false;
+			}
+
+			if (sfevent.type == sf::Event::KeyPressed) {
+				switch (sfevent.key.code) {
+				case sf::Keyboard::W: player.input.up    = true; break;
+				case sf::Keyboard::A: player.input.left  = true; break;
+				case sf::Keyboard::S: player.input.down  = true; break;
+				case sf::Keyboard::D: player.input.right = true; break;
+				default: /*jack shit i think???*/ break;
+				}
+			}
+			if (sfevent.type == sf::Event::KeyReleased) {
+				switch (sfevent.key.code) {
+			    case sf::Keyboard::W: player.input.up    = false; break;
+				case sf::Keyboard::A: player.input.left  = false; break;
+				case sf::Keyboard::S: player.input.down  = false; break;
+				case sf::Keyboard::D: player.input.right = false; break;
+				default: /*jack shit i think???*/ break;
+				}
+			}
+
+			if (sfevent.type == sf::Event::MouseButtonPressed) {
+				if (sfevent.key.code == sf::Mouse::Left) {
+					player.input.shoot = true;
+					//handle spawning bullet somewhere
+				}
+				if (sfevent.key.code == sf::Mouse::Right) {
+					player.input.shield = true;
+					//handle spawning shield somewhere
+				}
+			}
+			if (sfevent.type == sf::Event::MouseButtonReleased) {
+				if (sfevent.key.code == sf::Mouse::Left) {
+					player.input.shoot = false;
+					//don't shoot any longer, (what about holding key???)
+				}
+				if (sfevent.key.code == sf::Mouse::Right) {
+					player.input.shield = false;
+					//(think deactivate shield is fine as long as it is not destroyed, meaning mem is not released something something we'll see)
+				}
+			}
+		}
+
+	}
+
+	void sMovement() {
+		//also make sure everything else that moves, moves
+		//think that's it tho
+		for (auto& each : entities.GetEntities()) {
+			each.transform.pos += each.transform.vel;
+		}
+
+		static constexpr int temp_player_speed = 5;
+
+		player.transform.vel = { 0,0 };		//reset vel each frame
+
+		if (player.input.up) {
+			player.transform.vel.y -= temp_player_speed;
+		}
+		if (player.input.left) {
+			player.transform.vel.x -= temp_player_speed;
+		}
+		if (player.input.down) {
+			player.transform.vel.y += temp_player_speed;
+		}
+		if (player.input.right) {
+			player.transform.vel.x += temp_player_speed;
+		}
+
+
+
+		player.transform.pos += player.transform.vel;
+	}
+
+
 };
